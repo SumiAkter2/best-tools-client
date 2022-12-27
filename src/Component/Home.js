71279094f02468e1db31b32725ea9cd2bfb7300a
SumiAkter2 <akteralisha36@gmail.com>
2022-12-27 15:15:01 +0600

    added review page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import About from './About';
-import Arrival from './Arrival';
-import Bannar from './Bannar';
-import GetReview from './GetReview';
-import Hotdeels from './Hotdeels';
-import Products from './Products';
-import Reviews from './Reviews';
-import Subcrib from './Subcrib';
-import Summary from './Summary';
-
-
+import React from "react";
+import About from "./About";
+import Arrival from "./Arrival";
+import Bannar from "./Bannar";
+import GetReview from "./GetReview";
+import Hotdeels from "./Hotdeels";
+import Products from "./Products";
+import Reviews from "./Reviews";
+import Subcrib from "./Subcrib";
+import Summary from "./Summary";
 
 const Home = () => {
-    return (
-      <div>
-        <Bannar></Bannar>
-        <Products></Products>
-        {/*  <GetReview></GetReview>
-            <Reviews></Reviews>
+  return (
+    <div>
+      <Bannar></Bannar>
+      <Products></Products>
+      <GetReview></GetReview>
+      <Reviews></Reviews>
 
-            <Summary></Summary>
+      {/* <Summary></Summary>
             <About></About>
             <Hotdeels></Hotdeels>
             <Arrival></Arrival>
             <Subcrib></Subcrib> */}
-      </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
