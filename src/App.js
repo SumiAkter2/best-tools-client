fdc450ba283cdfac7c341d9f328ddf8bf9612aff
SumiAkter2 <akteralisha36@gmail.com>
2022-12-27 15:06:39 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import './App.css';
-import Navbar from './Component/Navbar/Navbar';
-import Home from './Component/Home';
-import { Route, Routes } from 'react-router-dom';
-import Blogs from './Component/Blogs';
-import LogIn from './Component/LogIn';
-import Footer from './Component/Footer';
-import Summary from './Component/Summary';
-import SignIn from './Component/SignIn';
-import RequireAuth from './Component/RequireAuth';
-import NotFound from './Component/NotFound';
-import DashBoard from './Component/DashBoard/DashBoard';
-import MyProfile from './Component/DashBoard/MyProfile';
-import MyOrder from './Component/DashBoard/MyOrder';
-import AddReview from './Component/DashBoard/AddReview';
-import ProductDetails from './Component/ProductDetails';
-import MyPortfolio from './Component/MyPortfolio';
-import { ToastContainer, toast } from 'react-toastify';
+import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
+import Home from "./Component/Home";
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./Component/Blogs";
+import LogIn from "./Component/LogIn";
+import Footer from "./Component/Footer";
+import Summary from "./Component/Summary";
+import SignIn from "./Component/SignIn";
+import RequireAuth from "./Component/RequireAuth";
+import NotFound from "./Component/NotFound";
+import DashBoard from "./Component/DashBoard/DashBoard";
+import MyProfile from "./Component/DashBoard/MyProfile";
+import MyOrder from "./Component/DashBoard/MyOrder";
+import AddReview from "./Component/DashBoard/AddReview";
+import ProductDetails from "./Component/ProductDetails";
+import MyPortfolio from "./Component/MyPortfolio";
+import { ToastContainer, toast } from "react-toastify";
 // import 'react-toastify/dist/ReactToastify.css';
-import useAdmin from './Hooks/UseAdmin';
+import useAdmin from "./Hooks/UseAdmin";
 // import AdminProfile from './Component/DashBoard/AdminProfile';
-import ManageAllProducts from './Component/DashBoard/ManageAllProducts';
-import MakeAdmin from './Component/DashBoard/MakeAdmin';
-import ManageOrder from './Component/DashBoard/ManageOrder';
-import AddProducts from './Component/DashBoard/AddProducts';
-import UpdateProfile from './Component/DashBoard/UpdateProfile';
-
+import ManageAllProducts from "./Component/DashBoard/ManageAllProducts";
+import MakeAdmin from "./Component/DashBoard/MakeAdmin";
+import ManageOrder from "./Component/DashBoard/ManageOrder";
+import AddProducts from "./Component/DashBoard/AddProducts";
+import UpdateProfile from "./Component/DashBoard/UpdateProfile";
 
 function App() {
   const [admin] = useAdmin();
   return (
     <div className="App">
       <Navbar>
-        {/* <Routes>
+        <Routes>
+          {/*
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='blogs' element={<Blogs></Blogs>}></Route>
           <Route path='login' element={<LogIn></LogIn>}></Route>
@@ -57,11 +57,11 @@ function App() {
             </Route>
           </Route>
           <Route path='summary' element={<Summary />}></Route>
-          <Route path='*' element={<NotFound />}></Route>
+          <Route path='*' element={<NotFound />}></Route>*/}
         </Routes>
-        <Footer></Footer>*/}
+        <Footer></Footer>
       </Navbar>
-      <ToastContainer></ToastContainer> 
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
